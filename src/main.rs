2c273b5ab1be5e32ace2201c5e2e18b923fb5fa6
maebee-cm <zee_cm@protonmail.com>
2022-12-30 18:30:15 -0700

    run command

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::io::Result;
+
+fn main() -> Result<()> {
+    // read env variables that were set in build script
+    let uefi_path = env!("UEFI_PATH");
+
+    let mut cmd = std::process::Command::new("qemu-system-x86_64");
+    cmd.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
+    cmd.arg("-drive")
+        .arg(format!("format=raw,file={uefi_path}"));
+    cmd.arg("-device")
+        .arg("isa-debug-exit,iobase=0xf4,iosize=0x04");
+
+    let mut child = cmd.spawn()?;
+    child.wait()?;
+    Ok(())
+}
